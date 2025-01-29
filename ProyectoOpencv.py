a56ff2a08fb678a6e022d22879cb93dd4b6e342b
CharlyBit <erickperezmendoza5@gmail.com>
2025-01-28 19:12:09 -0600

    Codigo actualizado (Compatible con Linux)

--- ProyectoOpencv.py
+++ ProyectoOpencv.py
@@ -4,21 +4,20 @@ import math
 import time
 import serial
 
-arduino = serial.Serial('COM4', 9600)
-
 # Se configura el dispositivo que captura el video
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+#arduino = serial.Serial('COM8', 9600)
 captura = cv2.VideoCapture(0)
-captura.set(3, 1280)  # Se define el ancho de la ventana
-
-captura.set(4, 720)  # Se define el alto de la ventana
+#captura.set(3, 1280)  # Se define el ancho de la ventana
+#captura.set(4, 720)  # Se define el alto de la ventana
+captura.set(3, 1100)  # Se define el ancho de la ventana
+captura.set(4, 600)  # Se define el alto de la ventana
 
 # Variables para conteo
 parpadeo = False
 conteo = 0
 tiempo = 0
-tiempo2 = 0
 inicio = 0
-inicio2 = 0
 final = 0
 conteo_sue = 0
 muestra = 0
@@ -36,8 +35,8 @@ mallaFacial = mediapipeMallaFacial.FaceMesh(
     min_tracking_confidence=0.5,
 )
 
-while captura.isOpened():
-    # Lee los valores del tamaño y calidad de la pantalla
+while captura.isOpened:
+
     ret, video = captura.read()
 
     cuadroRGB = cv2.cvtColor(video, cv2.COLOR_BGR2RGB)
@@ -59,7 +58,7 @@ while captura.isOpened():
                 landmark_drawing_spec=None,
                 connection_drawing_spec=mediapipeEstilos.get_default_face_mesh_tesselation_style(),
             )
- 
+
             for id, puntos in enumerate(rostros.landmark):
                 al, an, c = video.shape
                 x, y = int(puntos.x * an), int(puntos.y * al)
@@ -69,11 +68,11 @@ while captura.isOpened():
                 if len(lista) == 468:
                     x1, y1 = lista[145][1:]
                     x2, y2 = lista[159][1:]
-                    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2 
+                    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
                     longitud1 = math.hypot(x2 - x1, y2 - y1)
 
-                    x3, y3 = lista[374][1:]
-                    x4, y4 = lista[386][1:]
+                    x3, y3 = lista[145][1:]
+                    x4, y4 = lista[159][1:]
                     cx2, cy2 = (x3 + x4) // 2, (y3 + y4) // 2
                     longitud2 = math.hypot(x4 - x3, y4 - y3)
 
@@ -82,15 +81,14 @@ while captura.isOpened():
                         f"Parpadeos: {int(conteo)}",
                         (20, 60),
                         cv2.FONT_HERSHEY_SIMPLEX,
-                       1,
+                        1,
                         (0, 255, 0),
-                       2,
+                        2,
                     )
 
                     cv2.putText(
                         video,
-                        f"Micro suenos: {int(conteo_sue)}",
-                        #(20, 60),
+                        f"Micro sueños: {int(conteo_sue)}",
                         (350, 60),
                         cv2.FONT_HERSHEY_SIMPLEX,
                         1,
@@ -100,7 +98,7 @@ while captura.isOpened():
 
                     cv2.putText(
                         video,
-                        f"Duracion: {int(muestra)}",
+                        f"Duración: {int(muestra)}",
                         (160, 100),
                         cv2.FONT_HERSHEY_SIMPLEX,
                         1,
@@ -108,32 +106,49 @@ while captura.isOpened():
                         2,
                     )
 
-                    if longitud1 <= 15 and longitud2 <= 15:
-                       
-                        conteo +=1
+                    cv2.putText(
+                         video,
+                         f"longitud1: {int(longitud1)}",
+                         (20, 180),
+                         cv2.FONT_HERSHEY_SIMPLEX,
+                         1,
+                         (0, 255, 0),
+                         2,
+                     )
 
-                        if not parpadeo:
-                            inicio = time.time()
-                            parpadeo = True
-                            
-                        else:
-                            tiempo_cerrado = time.time() - inicio
-                            if tiempo_cerrado > 2:
-                                conteo_sue += 1
-                                muestra = tiempo_cerrado
-                                # Aqui seria la activacion del arduino
-                                arduino.write(b'a')
-                                inicio = time.time()
-                    elif longitud1 > 15 and longitud2 > 15 and parpadeo==True:
-                        # Aqui seria el apagado del sonido del arduino
-                        arduino.write(b'b')
-                        conteo +=1
-                    else:
+                    cv2.putText(
+                         video,
+                         f"longitud2: {int(longitud2)}",
+                         (350, 180),
+                         cv2.FONT_HERSHEY_SIMPLEX,
+                         1,
+                         (0, 255, 0),
+                         2,
+                     )
+
+                    if longitud1 <= 15 and longitud2 <= 15 and parpadeo == False:
+                        conteo += 1
+                        parpadeo = True
+                        inicio = time.time()
+                    elif longitud1 > 15 and longitud2 > 15 and parpadeo == True:
                         parpadeo = False
+                        final = time.time()
+                        arduino.write(b'b')
 
-    if ret:
-        cv2.imshow("Deteccion de sueno", video)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+                    tiempo = round(final - inicio, 0)
+
+                    if tiempo >= 3:
+                        conteo_sue += 1
+                        muestra = tiempo
+                        arduino.write(b'a')
+                        inicio = 0
+                        final = 0
+    
+    if (ret):
+       #cv2.imshow("Detección de sueño", video)
+       # video=cv2.flip(video,1)
+        cv2.imshow("Deteccion de sueño",video)
+    if cv2.waitKey(1)& 0xFF==ord("q"):
         break
 captura.release()
 cv2.destroyAllWindows()
